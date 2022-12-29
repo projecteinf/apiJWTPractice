7899e1464fa411080df2482ff69d2b7e28a239f3
projecteinf <projecteinf@boscdelacoma.cat>
2022-12-29 13:01:10 +0100

    bcrypt is not a function

--- authServer.js
+++ authServer.js
@@ -1,3 +1,5 @@
+// Configuració bàsica servei APIREST
+
 require("dotenv").config()
 const express = require('express')
 const app = express();
@@ -8,4 +10,21 @@ const port = process.env.TOKEN_SERVER_PORT
 
 app.listen(port, () =>{
     console.log(`Authorization Server running on ${port} listening`)
+})
+
+// Configuració autentificació 
+
+// Creació usuari amb ARRAY ( Cal fer-ho amb una BD!)
+
+const bcrypt = require("bcrypt")
+const users = []
+
+app.post("/createUser", async(req,res) => {
+    const user = req.body.name
+    const hashedPassword = await bcrypt(req.body.password,10)
+
+    users.push( {user: user, password: hashedPassword})
+
+    res.status(201).send(users)
+    console.log(users)
 })
