4956166a6f872da80a8b606764b883289d9aa01e
projecteinf <projecteinf@boscdelacoma.cat>
2022-12-29 22:28:21 +0100

    Canviat fitxer readme.md

--- authServer.js
+++ authServer.js
@@ -20,11 +20,19 @@ const bcrypt = require('bcrypt')
 const users = []
 
 app.post("/createUser", async(req,res) => {
+    console.log(req);
     const user = req.body.name
-    const hashedPassword = await bcrypt(req.body.password,10)
+    const salt = await bcrypt.genSalt(10);
+    
+    const hashedPassword = await bcrypt.hash(req.body.password,salt)
 
     users.push( {user: user, password: hashedPassword})
 
     res.status(201).send(users)
     console.log(users)
+})
+
+app.get("/createUser", async(req,res) => {
+    res.status(201).send({"state":"OK"})
+    console.log("Working!")
 })
